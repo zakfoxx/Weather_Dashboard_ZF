558f336ab7028113e708b64eaf3b997e702fa19c
Zechariah Foxx <zak.foxx@gmail.com>
2022-07-12 18:43:31 -0400

    add lat and lon

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,8 @@
 var apiKey = "b06f6c7b10cb4150cdf88358e4b7eaed";
 
 function apiCall() {
-  var lat;
-  var lon;
+  var lat = "36";
+  var lon = "-78";
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
     lat +
@@ -21,10 +21,7 @@ function apiCall() {
 }
 apiCall();
 
-// get api key working
-// lookup lat and lon for durham on google.maps - set lines 4 and 5 to durham
 // google openweather api for city lookup - use syntax from line 6 for specific call - should give input for city name's lat and lon
 // define lat/lon variables for city's data
 // copy/paste line 14 to run new city to lookup lat/lon weather
 // save to localStorage and display on HTML - display weather for city lookup and 5 day forecast
-
