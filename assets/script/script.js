d8920881407adc0494d2b3257b40779c262ffc46
Zechariah Foxx <zak.foxx@gmail.com>
2022-07-12 22:03:15 -0400

    created API fetch

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,9 @@
 var apiKey = "b06f6c7b10cb4150cdf88358e4b7eaed";
-
+var searchButton = document.querySelector(".submit-button");
+var citySearch = document.querySelector(".city-search");
 function apiCall() {
-  var lat = "36";
-  var lon = "-78";
+  var lat = data[0];
+  var lon = "";
   var requestUrl =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
     lat +
@@ -19,7 +20,42 @@ function apiCall() {
       console.log(data);
     });
 }
-apiCall();
+searchButton.addEventListener("click", function () {
+  console.log(citySearch.value);
+  var citySearchUrl =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    citySearch.value +
+    "&appid=" +
+    apiKey;
+  fetch(citySearchUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data[0].lat);
+      console.log(data[0].lon);
+      var lat = data[0].lat;
+      var lon = data[0].lon;
+      var requestUrl =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        lat +
+        "&lon=" +
+        lon +
+        "&units=imperial&appid=" +
+        apiKey;
+
+      fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        });
+    });
+});
+
+// http://api.openweathermap.org/geo/1.0/direct?q={searchCity.value},{state code},{country code}&limit={limit}&appid={API key}
 
 // google openweather api for city lookup - use syntax from line 6 for specific call - should give input for city name's lat and lon
 // define lat/lon variables for city's data
