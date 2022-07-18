478b3b44fe2a8210df31ea2c2191d99425cb59d0
Zechariah Foxx <zak.foxx@gmail.com>
2022-07-18 15:59:30 -0400

    html and css

--- assets/script/script.js
+++ assets/script/script.js
@@ -50,7 +50,7 @@ searchButton.addEventListener("click", function () {
           return response.json();
         })
         .then(function (data) {
-          console.log(data);
+          console.log("line 53", data);
         });
     });
 });
@@ -61,3 +61,17 @@ searchButton.addEventListener("click", function () {
 // define lat/lon variables for city's data
 // copy/paste line 14 to run new city to lookup lat/lon weather
 // save to localStorage and display on HTML - display weather for city lookup and 5 day forecast
+
+// create html elements displaying weather data -
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
